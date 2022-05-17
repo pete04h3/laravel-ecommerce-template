a6d7963056ed8c08afa1cb408aefc9c62f922a83
Peter <47736475+pete04h3@users.noreply.github.com>
2022-05-17 09:06:05 +0200

    Updated homecomponent

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -22,8 +22,8 @@ class HomeComponent extends Component
         $latest_products = Product::orderBy('created_at', 'DESC')->get()->take(8);
         $popular_products = Product::inRandomOrder()->get()->take(8);
         $category = HomeCategory::find(1);
-        //$cats = explode(',', $category->sel_categories);
-        $categories = Category::whereIn('id')->get();
+        $cats = explode(',', $category->sel_categories);
+        $categories = Category::whereIn('id, $cats')->get();
         $no_of_products = $category->no_of_products;
         $sproducts = Product::where('sale_price', '>', 0)->inRandomOrder()->get()->take(8);
         $ahrefproducts = Product::inRandomOrder()->limit(5)->get()->take(5);
